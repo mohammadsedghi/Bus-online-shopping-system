6cd5ca6fac58ade56d5a799e6a8a5d4625bcea26
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-07-09 20:22:25 +0330

    wrong item is corrected,signup html,css is created

--- src/main/java/ir/maktab/mohammad_sedghi_hw23_question1_maktab92/entity/Passenger.java
+++ src/main/java/ir/maktab/mohammad_sedghi_hw23_question1_maktab92/entity/Passenger.java
@@ -2,7 +2,11 @@ package ir.maktab.mohammad_sedghi_hw23_question1_maktab92.entity;
 
 import ir.maktab.mohammad_sedghi_hw23_question1_maktab92.base.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 @Setter
 @Getter
@@ -10,6 +14,7 @@ import lombok.*;
 @NoArgsConstructor
 @ToString
 @Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Passenger extends BaseEntity<Long> {
     String firstName;
     String lastName;
--- src/main/java/ir/maktab/mohammad_sedghi_hw23_question1_maktab92/util/HibernateUtil.java
+++ src/main/java/ir/maktab/mohammad_sedghi_hw23_question1_maktab92/util/HibernateUtil.java
@@ -1,5 +1,6 @@
 package ir.maktab.mohammad_sedghi_hw23_question1_maktab92.util;
 
+import ir.maktab.mohammad_sedghi_hw23_question1_maktab92.entity.Passenger;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
@@ -7,12 +8,15 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateUtil {
+
+
     public static final StandardServiceRegistry standardServiceRegistry = new StandardServiceRegistryBuilder()
             .configure("hibernate.cfg.xml").build();
     public static final Metadata metadata = new MetadataSources(standardServiceRegistry).getMetadataBuilder().build();
     public static final SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
 
     public static SessionFactory getSessionFactory() {
+
         return sessionFactory;
     }
 }
