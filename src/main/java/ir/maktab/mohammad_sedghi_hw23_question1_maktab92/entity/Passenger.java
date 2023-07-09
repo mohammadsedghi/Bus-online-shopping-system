1b41452cc08a608546e088d17adf87b1f8aeefcb
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-07-09 14:20:06 +0330

    home html,css search html,css and login html is written

--- src/main/java/ir/maktab/mohammad_sedghi_hw23_question1_maktab92/entity/Passenger.java
+++ src/main/java/ir/maktab/mohammad_sedghi_hw23_question1_maktab92/entity/Passenger.java
@@ -1,6 +1,21 @@
 package ir.maktab.mohammad_sedghi_hw23_question1_maktab92.entity;
 
 import ir.maktab.mohammad_sedghi_hw23_question1_maktab92.base.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.*;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
 public class Passenger extends BaseEntity<Long> {
+    String firstName;
+    String lastName;
+    String nationalId;
+    String gender;
+    String username;
+    String password;
+
 }
--- src/main/java/ir/maktab/mohammad_sedghi_hw23_question1_maktab92/entity/Ticket.java
+++ src/main/java/ir/maktab/mohammad_sedghi_hw23_question1_maktab92/entity/Ticket.java
@@ -1,6 +1,27 @@
 package ir.maktab.mohammad_sedghi_hw23_question1_maktab92.entity;
 
 import ir.maktab.mohammad_sedghi_hw23_question1_maktab92.base.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Ticket extends BaseEntity<Long> {
+    @ManyToOne
+    Passenger passenger;
+    String from;
+    String destination;
+    LocalDate departureDate;
+    String departureTime;
+    String travelId;
+
 }
